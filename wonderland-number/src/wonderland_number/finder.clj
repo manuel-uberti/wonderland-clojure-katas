24bdc00256ef0a78d44b89316275eda4eab25954
Manuel <manuel@boccaperta.com>
2015-06-03 09:14:22 +0200

    Add wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,21 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [x y]
+  (= (set (str x)) (set (str y))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [n 100000]
+    (let [d (* n 2)
+          t (* n 3)
+          q (* n 4)
+          c (* n 5)
+          s (* n 6)]
+      (if (and (same-digits? n d)
+               (same-digits? n t)
+               (same-digits? n q)
+               (same-digits? n c)
+               (same-digits? n s))
+        n
+        (recur (inc n))))))
+
+(wonderland-number)
